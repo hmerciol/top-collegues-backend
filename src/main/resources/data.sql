103082c4aa51b2e65fa15f4e149dcfebe42da57a
hmerciol <li.henri@merciol.fr>
2018-02-15 12:10:51 +0100

    images

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO COLLEGUE (pseudo,image_url,score)
-	VALUES ('Joe','',15),('William','',65),('Jack','',65),('Averel','',95);
+	VALUES ('Joe','http://www.lucky-luke.com/en-uploads/characters/Jo.jpg',15),
+	('William','http://www.lucky-luke.com/en-uploads/characters/william.jpg',65),
+	('Jack','http://www.lucky-luke.com/en-uploads/characters/Jack.jpg',65),
+	('Averel','http://www.lucky-luke.com/fr-uploads/personnages/averell.jpg',95);
