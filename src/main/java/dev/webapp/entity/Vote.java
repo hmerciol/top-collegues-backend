4a09a47f6c28eddb20f873bab6003d436b77a0cd
hmerciol <li.henri@merciol.fr>
2018-02-21 10:34:30 +0100

    votes enregistrés dans BDD

--- src/main/java/dev/webapp/entity/Vote.java
+++ src/main/java/dev/webapp/entity/Vote.java
@@ -1,22 +1,52 @@
 package dev.webapp.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Vote {
 	
-	private static int compteur = 0;
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@ManyToOne
 	private Collegue collegue;
 	private String avis;
 	
+	/**
+	 * 
+	 */
+	public Vote() {
+		super();
+	}
+
 	/**
 	 * @param collegue
 	 * @param avis
 	 */
 	public Vote(Collegue collegue, String avis) {
 		super();
-		this.id = ++compteur;
 		this.collegue = collegue;
 		this.avis = avis;
+	}
+
+	/**
+	 * Getter for the id
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * Setter for the id
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	/**
@@ -28,6 +58,14 @@ public class Vote {
 	}
 
 	/**
+	 * Setter for the collegue
+	 * @param collegue the collegue to set
+	 */
+	public void setCollegue(Collegue collegue) {
+		this.collegue = collegue;
+	}
+
+	/**
 	 * Getter for the avis
 	 * @return the avis
 	 */
@@ -36,11 +74,11 @@ public class Vote {
 	}
 
 	/**
-	 * Getter for the id
-	 * @return the id
+	 * Setter for the avis
+	 * @param avis the avis to set
 	 */
-	public Integer getId() {
-		return id;
+	public void setAvis(String avis) {
+		this.avis = avis;
 	}
 
 }
